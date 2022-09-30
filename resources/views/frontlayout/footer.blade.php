9385419ee1705051411fd0411d26631c16a02f8a
Atif Raza <layyahboy111@gmail.com>
2022-09-30 15:39:41 +0500

    Atif

---
+++ resources/views/frontlayout/footer.blade.php
@@ -0,0 +1,68 @@
+   <!-- Concatenated js plugins and jQuery -->
+    <script src="assets/js/app.js"></script>
+    <script src="https://js.stripe.com/v3/"></script>
+    <script src="assets/data/tipuedrop_content.js"></script>
+
+    <!-- Core js -->
+    <script src="assets/js/global.js"></script>
+
+    <!-- Navigation options js -->
+    <script src="assets/js/navbar-v1.js"></script>
+    <script src="assets/js/navbar-v2.js"></script>
+    <script src="assets/js/navbar-mobile.js"></script>
+    <script src="assets/js/navbar-options.js"></script>
+    <script src="assets/js/sidebar-v1.js"></script>
+
+    <!-- Core instance js -->
+    <script src="assets/js/main.js"></script>
+    <script src="assets/js/chat.js"></script>
+    <script src="assets/js/touch.js"></script>
+    <script src="assets/js/tour.js"></script>
+
+    <!-- Components js -->
+    <script src="assets/js/explorer.js"></script>
+    <script src="assets/js/widgets.js"></script>
+    <script src="assets/js/modal-uploader.js"></script>
+    <script src="assets/js/popovers-users.js"></script>
+    <script src="assets/js/popovers-pages.js"></script>
+    <script src="assets/js/lightbox.js"></script>
+
+    <!-- Landing page js -->
+
+    <!-- Signup page js -->
+
+    <!-- Feed pages js -->
+    <script src="assets/js/feed.js"></script>
+
+    <script src="assets/js/webcam.js"></script>
+    <script src="assets/js/compose.js"></script>
+    <script src="assets/js/autocompletes.js"></script>
+
+    <!-- profile js -->
+
+    <!-- stories js -->
+
+    <!-- friends js -->
+
+    <!-- questions js -->
+
+    <!-- video js -->
+
+    <!-- events js -->
+
+    <!-- news js -->
+
+    <!-- shop js -->
+
+    <!-- inbox js -->
+
+    <!-- settings js -->
+
+    <!-- map page js -->
+
+    <!-- elements page js -->
+</body>
+
+
+<!-- Mirrored from friendkit.cssninja.io/navbar-v1-feed.html by HTTrack Website Copier/3.x [XR&CO'2014], Wed, 10 Aug 2022 09:17:28 GMT -->
+</html>
